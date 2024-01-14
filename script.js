673b54fea6604b426014b35ede85a1a79daa0d9f
MariannaHren <146820222+MariannaHren@users.noreply.github.com>
2024-01-14 23:04:20 +0300

    Домашнее задание 11

---
+++ script.js
@@ -0,0 +1,44 @@
+let word = document.querySelector(".word");
+
+const arrayOfWords = [
+  "гимназист",
+  "стелька",
+  "подделка",
+  "виза",
+  "решение",
+  "шуруп",
+  "колодец",
+  "кабан",
+  "команда",
+  "ловушка",
+  "буква",
+  "опера",
+  "сектор",
+  "математика",
+  "пароварка",
+  "глубина",
+  "справочник",
+  "вождь",
+  "хобот",
+];
+
+function getrandom(min, max) {
+  let random = Math.random() * (max - min) + min;
+  return +random.toFixed(0);
+}
+let randomWord = arrayOfWords[getrandom(0, arrayOfWords.length)];
+
+let byLetter = randomWord
+  .split("")
+  .map((item) => `<span>${item}</span>`)
+  .join("");
+word.innerHTML = byLetter;
+const elements = document.querySelectorAll(".word span");
+let i = 0;
+document.addEventListener("keydown", function (event) {
+  if (event.key === randomWord[i]) {
+    elements[i].classList.toggle("c");
+  } else if (event.key != randomWord[i]) {
+    elements[i].classList.toggle("w");
+  }
+});
